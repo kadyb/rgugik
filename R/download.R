adcdf90fc5793974e7ec9f6bfc4e949fce31b9a8
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-13 16:38:56 +0200

    iterate over geometries

--- R/download.R
+++ R/download.R
@@ -3,9 +3,9 @@ request = function(polygon, composition = NULL, year = NULL, resolution = NULL) 
   require("jsonlite")
 
   if (!is.null(composition) && !(composition %in% c("B/W", "RGB", "CIR"))) stop("incorrect composition (should be 'B/W', 'RGB' or 'CIR'")
+  if (nrow(polygon) == 0) stop("no geometries")
 
   epsg = st_crs(polygon)$epsg
-  bbox = st_bbox(polygon)
 
   # hard coded URL and parameters
   base_URL = "https://mapy.geoportal.gov.pl/gprest/services/SkorowidzeFOTOMF/MapServer/0/query?"
@@ -15,13 +15,43 @@ request = function(polygon, composition = NULL, year = NULL, resolution = NULL) 
   returnGeometry = "&returnGeometry=false"
   file = "&f=json"
 
-  # user input
-  geometry = paste0("geometry={'xmin':", bbox[1], ", 'ymin':", bbox[2], ", 'xmax':", bbox[3], ", 'ymax':", bbox[4], ", 'spatialReference':{'wkid':", epsg, "}}")
+  # initial empty df
+  empty_df = data.frame(godlo = character(),
+                        akt_rok = integer(),
+                        piksel = numeric(),
+                        kolor = character(),
+                        zrDanych = character(),
+                        ukladXY = character(),
+                        modulArch = character(),
+                        nrZglosz = character(),
+                        czy_ark_wypelniony = character(),
+                        daneAktualne = integer(),
+                        daneAktDo10cm = integer(),
+                        lok_orto = character(),
+                        url_do_pobrania = character(),
+                        idSerie = integer(),
+                        sha1 = character(),
+                        akt_data = numeric(),
+                        idorto = integer(),
+                        nazwa_pliku = character(),
+                        ESRI_OID = integer()
+  )
 
-  prepared_URL = paste0(base_URL, geometry, geometryType, spatialRel, outFields,
-                        returnGeometry, file)
+  for (i in 1:nrow(polygon)) {
+    bbox = st_bbox(st_geometry(polygon)[[i]])
 
-  outputJSON = fromJSON(prepared_URL)
-  return(outputJSON$features[[1]])
+    # user input
+    geometry = paste0("geometry={'xmin':", bbox[1], ", 'ymin':", bbox[2], ", 'xmax':", bbox[3], ", 'ymax':", bbox[4], ", 'spatialReference':{'wkid':", epsg, "}}")
+
+    prepared_URL = paste0(base_URL, geometry, geometryType, spatialRel, outFields,
+                          returnGeometry, file)
+
+    output = fromJSON(prepared_URL)
+    output = output$features[[1]]
+    empty_df = rbind(empty_df, output)
+  }
+
+  # remove duplicated images
+  empty_df = empty_df[!duplicated(empty_df$nazwa_pliku), ]
+  return(empty_df)
 }
-
