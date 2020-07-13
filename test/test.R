2a8a05b3f59ade882fd0b7a3f48046fce81497f3
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-13 18:05:07 +0200

    update test

--- test/test.R
+++ test/test.R
@@ -2,8 +2,15 @@ library("sf")
 
 polygon = read_sf("poligon.gpkg")
 
-req_df = request(polygon = polygon)
-str(req_df)
+
+req_df1 = request(polygon = polygon)
+
+req_df2 = request(polygon = polygon, where = "kolor LIKE 'CIR'")
+
+req_df3 = request(polygon = polygon, where = "piksel <= 0.25")
+
+req_df4 = request(polygon = polygon, where = "akt_rok >= 2016")
+
 
 # download only first image
-download.file(req_df[1, "url_do_pobrania"], paste0(req_df[1, "nazwa_pliku"], ".tif"), mode = "wb")
+download.file(req_df1[1, "url_do_pobrania"], paste0(req_df[1, "nazwa_pliku"], ".tif"), mode = "wb")
