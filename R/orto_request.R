6379e2647e36c1b80ef4962982e98f101f0a3c5b
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-15 11:29:44 +0200

    output columns selection

--- R/orto_request.R
+++ R/orto_request.R
@@ -3,13 +3,17 @@ orto_request = function(polygon, where = NULL) {
   if (!is.null(where) && !is.character(where)) stop("'where' must be string")
   if (nrow(polygon) == 0) stop("no geometries")
 
+  selected_cols = c("godlo", "akt_rok", "piksel", "kolor", "zrDanych",
+                  "url_do_pobrania", "idSerie", "sha1", "nazwa_pliku")
+  selected_cols = paste(selected_cols, collapse = ",")
+
   epsg = sf::st_crs(polygon)$epsg
 
   # hard coded URL and parameters
   base_URL = "https://mapy.geoportal.gov.pl/gprest/services/SkorowidzeFOTOMF/MapServer/0/query?"
   geometryType = "&geometryType=esriGeometryEnvelope"
   spatialRel = "&spatialRel=esriSpatialRelIntersects"
-  outFields = "&outFields=*"
+  outFields = paste0("&outFields=", selected_cols)
   returnGeometry = "&returnGeometry=false"
   file = "&f=json"
 
@@ -19,26 +23,26 @@ orto_request = function(polygon, where = NULL) {
     empty_where = paste0(empty_where, where)
   }
 
-  # initial empty df
+  # initial empty df (columns must be identical as in 'selected_cols')
   empty_df = data.frame(godlo = character(),
                         akt_rok = integer(),
                         piksel = numeric(),
                         kolor = character(),
                         zrDanych = character(),
-                        ukladXY = character(),
-                        modulArch = character(),
-                        nrZglosz = character(),
-                        czy_ark_wypelniony = character(),
-                        daneAktualne = integer(),
-                        daneAktDo10cm = integer(),
-                        lok_orto = character(),
+                        #ukladXY = character(),
+                        #modulArch = character(),
+                        #nrZglosz = character(),
+                        #czy_ark_wypelniony = character(),
+                        #daneAktualne = integer(),
+                        #daneAktDo10cm = integer(),
+                        #lok_orto = character(),
                         url_do_pobrania = character(),
                         idSerie = integer(),
                         sha1 = character(),
-                        akt_data = numeric(),
-                        idorto = integer(),
-                        nazwa_pliku = character(),
-                        ESRI_OID = integer()
+                        #akt_data = numeric(),
+                        #idorto = integer(),
+                        nazwa_pliku = character()
+                        #ESRI_OID = integer()
   )
 
   for (i in 1:nrow(polygon)) {
