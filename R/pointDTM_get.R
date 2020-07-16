40cf79808774dbaab2ccd768b0f7aabd6f6459c6
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-16 16:52:16 +0200

    try loop in 'pointDTM_get'

--- R/pointDTM_get.R
+++ R/pointDTM_get.R
@@ -35,28 +35,47 @@ pointDTM_get = function(polygon) {
 #############
 ### test
 
+base_URL = "https://services.gugik.gov.pl/nmt/?request=GetHByPointList&list="
+
 library("sf")
 polygon = read_sf("poligon.gpkg")
 pts = st_make_grid(polygon, cellsize = 1, what = "corners")
 pts = st_coordinates(pts)
 pts = apply(pts, 2, as.integer)
-pts = pts[1:500, ] # can use only 500 points
 
-f = paste(pts[, 1], pts[, 2])
-f = paste(f, collapse = ",")
+# initial empty character vector
+empty_output = character()
 
-base_URL = "https://services.gugik.gov.pl/nmt/?request=GetHByPointList&list="
-prepared_URL = paste0(base_URL, f)
-
-
-str_output = readLines(prepared_URL, warn = FALSE)
-str_output = unlist(strsplit(str_output, ","))
-str_output = unlist(strsplit(str_output, " "))
-elev = str_output[1:(length(str_output)/3)*3] # take every 3rd element
-elev = as.numeric(elev)
-
+# request may only contain 500 points
 iter = ceiling(nrow(pts) / 500)
+n_last = nrow(pts) %% 500
 
-for (i in 1:iter) {
-  
+for (i in 0:iter) {
+  if (i < iter) {
+    sel_pts = pts[(i * 500 + 1):(i * 500 + 500), ]
+    str_pts = paste(sel_pts[, 1], sel_pts[, 2])
+    str_pts = paste(str_pts, collapse = ",")
+    prepared_URL = paste0(base_URL, str_pts)
+    str_output = readLines(prepared_URL, warn = FALSE)
+    str_output = unlist(strsplit(str_output, ","))
+    str_output = unlist(strsplit(str_output, " "))
+    elev = str_output[1:(length(str_output) / 3) * 3] # take every 3rd element (elevation)
+    empty_output = c(empty_output, elev)
+  } else {
+    sel_pts = pts[(i * 500 + 1):(i * 500 + n_last), ]
+    str_pts = paste(sel_pts[, 1], sel_pts[, 2])
+    str_pts = paste(str_pts, collapse = ",")
+    prepared_URL = paste0(base_URL, str_pts)
+    str_output = readLines(prepared_URL, warn = FALSE)
+    str_output = unlist(strsplit(str_output, ","))
+    str_output = unlist(strsplit(str_output, " "))
+    elev = str_output[1:(length(str_output) / 3) * 3] # take every 3rd element (elevation)
+    empty_output = c(empty_output, elev)
+  }
+  Sys.sleep(2) # wait 2 sec
 }
+
+length(empty_output) == nrow(pts) # WRONG!
+empty_output = as.numeric(elev)
+
+
