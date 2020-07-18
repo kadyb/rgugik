b931b6bb15612705d101968f438488bc445a6a45
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-18 16:44:44 +0200

    add SHA1 checksum check

--- R/orto_download.R
+++ R/orto_download.R
@@ -1,17 +1,35 @@
-orto_download = function(df_req, ...) {
+orto_download = function(df_req, check_SHA = FALSE, ...) {
 
-  if (!all(c("url_do_pobrania", "nazwa_pliku") %in% names(df_req))){
+  if (!all(c("url_do_pobrania", "nazwa_pliku") %in% names(df_req))) {
     stop("data frame should come from 'request_orto'")
   }
-  if (nrow(df_req) == 0){
+  
+  if (check_SHA == TRUE && !"sha1" %in% names(df_req)) {
+    stop("'sha1' column not found")
+  }
+  
+  if (nrow(df_req) == 0) {
     stop("empty df")
   }
 
-  for (i in 1:nrow(df_req)) {
-    utils::download.file(df_req[i, "url_do_pobrania"],
-                  paste0(df_req[i, "nazwa_pliku"], ".tif"),
-                  mode = "wb",
-                  ...)
+  if (!check_SHA) {
+    # only download files
+    for (i in 1:nrow(df_req)) {
+      name_file = paste0(df_req[i, "nazwa_pliku"], ".tif")
+      utils::download.file(df_req[i, "url_do_pobrania"], name_file, mode = "wb", ...)
+    }
+  } else {
+    # download files and check their checksum
+    for (i in 1:nrow(df_req)) {
+      name_file = paste0(df_req[i, "nazwa_pliku"], ".tif")
+      utils::download.file(df_req[i, "url_do_pobrania"], name_file, mode = "wb", ...)
+      # reference checksum is SHA-1
+      tmp_SHA = as.character(openssl::sha1(file(name_file)))
+      
+      if (!tmp_SHA == df_req[i, "sha1"]) {
+        warning(paste(name_file, "incorrect SHA"), immediate. = TRUE)
+      }
+    }
   }
-
+  
 }
