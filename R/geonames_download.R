f3a6dfd03e38d5fde35db95351443f03e447a4ef
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-23 20:07:16 +0200

    add State Register of Geographical Names

---
+++ R/geonames_download.R
@@ -0,0 +1,47 @@
+#' downloads State Register of Geographical Names
+#'
+#' @param type names of places ("place") and/or physiographic objects ("object")
+#' @param format data format ("GML", "SHP" and/or "XLSX")
+#' @param ...
+#'
+#' @return a selected data type in the specified format
+#' 
+#' @export
+#' 
+#' @examples
+#' geonames_download(type = c("place", "object"), format = "SHP")
+#' geonames_download(type = "object", format = c("GML", "XSLX"))
+geonames_download = function(type, format, ...) {
+  
+  if (!any(type == "place" || type == "object")) {
+    stop("incorrect type, should be 'place' or 'object'")
+  }
+  
+  if (!any(format == "GML" || format == "SHP" || format == "XLSX")) {
+    stop("incorrect format, should be 'GML', 'SHP' or 'XLSX'")
+  }
+  
+  formats = c("GML", "SHP", "XLSX")
+  types = c("place", "object")
+  place_URL = "ftp://91.223.135.109/prng/PRNG_MIEJSCOWOSCI_" # missing format
+  object_URL = "ftp://91.223.135.109/prng/PRNG_OBIEKTY_FIZJOGRAFICZNE_" # missing format
+  
+  # prepare correct URLs
+  df = data.frame(type = rep(types, each = 3),
+                  format = rep(formats, times = 2),
+                  URL = c(paste0(place_URL, formats, ".zip"),
+                          paste0(object_URL, formats, ".zip"))
+                  )
+  
+  sel = (df[, "type"] %in% type) * (df[, "format"] %in% format)
+  sel = as.logical(sel)
+  
+  df = df[sel, ]
+  
+  for (i in seq_len(nrow(df))) {
+    filename = paste0(df[i, "type"], "_", df[i, "format"], ".zip")
+    utils::download.file(df[i, "URL"], filename, mode = "wb", ...)
+    utils::unzip(filename)
+  }
+  
+}
