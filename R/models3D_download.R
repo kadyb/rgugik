8c638e341edf4ac49360eca34376e05f577d7838
kadyb <35004826+kadyb@users.noreply.github.com>
2020-07-30 17:03:34 +0200

    add new function "models3D_download"

---
+++ R/models3D_download.R
@@ -0,0 +1,73 @@
+#' downloads 3D models of buildings for counties
+#'
+#' @param county county name in Polish
+#' @param TERYT county ID (4 characters)
+#' @param LOD Level of detail for building models ("LOD1" or "LOD2", "LOD1" is default).
+#' "LOD2" is only available for ten voivodeships
+#' (TERC: "04", "06", "12", "14", "16", "18", "20", "24", "26", "28").
+#' Check 'voivodeships_names' function.
+#' @param ...
+#'
+#' @return models of buildings in Geography Markup Language format (.GML)
+#' 
+#' @export
+#' 
+#' @examples
+#' models3D_download(county = "Toruń")
+#' models3D_download(TERYT = c("2462", "0401"), LOD = "LOD2")
+models3D_download = function(county = NULL, TERYT = NULL, LOD = "LOD1", ...) {
+  
+  df_names = read.csv("supp/TERYT_county.csv", colClasses = c("TERYT" = "character"))
+  
+  if (is.null(county) && is.null(TERYT)) {
+    stop("'county' and 'TERYT' are empty")
+  }
+  
+  if (!is.null(county) && !is.null(TERYT)) {
+    stop("use only one input")
+  }
+  
+  if (!all(county %in% df_names$NAZWA)) {
+    stop("incorrect county name")
+  }
+  
+  if (!is.null(TERYT) && any(nchar(TERYT) != 4)) {
+    stop("incorrect TERYT")
+  }
+  
+  if (!LOD %in% c("LOD1", "LOD2")) {
+    stop("inncorect LOD, should be 'LOD1' or 'LOD2'")
+  }
+  
+  if (LOD == "LOD1") {
+    base_URL = "https://integracja.gugik.gov.pl/Budynki3D/pobierz.php?plik=powiaty/lod1/"
+  } else {
+    # LOD 2
+    base_URL = "https://integracja.gugik.gov.pl/Budynki3D/pobierz.php?plik=powiaty/"
+  }
+  
+  if (!is.null(county)) {
+    sel_vector = df_names[, "NAZWA"] %in% county
+    df_names = df_names[sel_vector, ]
+  } else {
+    sel_vector = df_names[, "TERYT"] %in% TERYT
+    df_names = df_names[sel_vector, ]
+  }
+  
+  # detect missing LOD2 counties
+  if (LOD == "LOD2" && sum(df_names$LOD2) != nrow(df_names)) {
+    warning("LOD2 is unavibile, trying drop missing counties", immediate. = TRUE)
+    df_names = df_names[df_names$LOD2 == TRUE, ]
+    if (nrow(df_names == 0)) {
+      stop("LOD2 is unavibile, use 'LOD1'")
+    }
+  }
+  
+  for (i in seq_len(nrow(df_names))) {
+    prepared_URL = paste0(base_URL, df_names[i, "TERYT"], "_gml.zip")
+    filename = paste0(df_names[i, "TERYT"], "_gml.zip")
+    utils::download.file(prepared_URL, filename, mode = "wb", ...)
+    utils::unzip(filename)
+  }
+  
+}
