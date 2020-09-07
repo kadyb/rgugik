cb14c1129de84853f54e3bd953938e96fe853e39
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-09-07 15:23:29 +0200

    add new function "topodb_download"

---
+++ R/topodb_download.R
@@ -0,0 +1,65 @@
+#' downloads Topographic Databases for counties
+#'
+#' @param county county name in Polish. Check [`county_names()`] function.
+#' @param TERYT county ID (4 characters)
+#' @param outdir (optional) name of the output directory;
+#' by default, files are saved in the working directory
+#' @param unzip TRUE (default) or FALSE, when TRUE the downloaded archive will
+#' be extracted and removed
+#' @param ... additional argument for [`utils::download.file()`]
+#'
+#' @return a database in Geography Markup Language format (.GML),
+#' the content and detail level corresponds to the topographic map
+#' in the scale of 1:10000
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' topodb_download(county = "Toruń")
+#' topodb_download(TERYT = c("2462", "0401"))
+#' }
+topodb_download = function(county = NULL, TERYT = NULL, outdir = ".",
+                             unzip = TRUE, ...) {
+
+  df_names = rgugik::county_names
+
+  if (is.null(county) && is.null(TERYT)) {
+    stop("'county' and 'TERYT' are empty")
+  }
+
+  if (!is.null(county) && !is.null(TERYT)) {
+    stop("use only one input")
+  }
+
+  if (!all(county %in% df_names$NAME)) {
+    stop("incorrect county name")
+  }
+
+  if (!is.null(TERYT) && any(nchar(TERYT) != 4)) {
+    stop("incorrect TERYT")
+  }
+
+  base_URL = "https://integracja.gugik.gov.pl/PRG/pobierz.php?bdot10k&teryt="
+
+  if (!is.null(county)) {
+    sel_vector = df_names[, "NAME"] %in% county
+    df_names = df_names[sel_vector, ]
+  } else {
+    sel_vector = df_names[, "TERYT"] %in% TERYT
+    df_names = df_names[sel_vector, ]
+  }
+
+  if (!dir.exists(outdir)) dir.create(outdir)
+
+  for (i in seq_len(nrow(df_names))) {
+    prepared_URL = paste0(base_URL, df_names[i, "TERYT"])
+    filename = paste0(outdir, "/", df_names[i, "TERYT"], ".zip")
+    utils::download.file(prepared_URL, filename, mode = "wb", ...)
+    if (unzip) {
+      utils::unzip(filename, exdir = outdir)
+      file.remove(filename)
+    }
+  }
+
+}
