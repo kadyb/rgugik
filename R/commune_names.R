f786285a3e59ec8fa12c0a0a5b1865440736ef18
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-09-17 13:12:44 +0200

    communes in Poland dataset

---
+++ R/commune_names.R
@@ -0,0 +1,13 @@
+#' @name commune_names
+#' @title Communes in Poland
+#'
+#' @description The data frame contains names of communes,
+#' and their identifiers (TERC, 7 characters).
+#'
+#' @importFrom utils data
+#'
+#' @docType data
+#' @keywords dataset commune
+#' @examples
+#' commune_names
+"commune_names"
