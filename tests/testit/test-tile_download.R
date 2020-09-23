b4fbb69c9fd7d70f5f7d6037daa22d21977c309e
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-09-23 12:43:35 +0200

    update test (will fail)

--- tests/testit/test-tile_download.R
+++ tests/testit/test-tile_download.R
@@ -3,17 +3,19 @@ library(testit)
 base_URL = "https://opendata.geoportal.gov.pl/"
 
 sample_orto = paste0(base_URL, "ortofotomapa/41/41_3756_N-33-130-D-b-2-3.tif")
-orto = data.frame(URL = sample_orto)
+orto = data.frame(URL = sample_orto, filename = "sample_orto",
+                  sha1 = "312c81963a31e268fc20c442733c48e1aa33838f")
 
 sample_DEM = paste0(base_URL, "NumDaneWys/NMT/73556/73556_1002897_N-33-130-D-b-2-3.asc")
-DEM = data.frame(URL = sample_DEM)
+DEM = data.frame(URL = sample_DEM, filename = "sample_DEM",
+                 sha1 = "392a0edf763e38fa1b7b6067ac1b080c47374dd2")
 
 # ORTO
 tmp = tempfile()
 tile_download(orto, outdir = tmp)
 file_path = list.files(tmp, full.names = TRUE)
 file_size = file.info(file_path)$size / 2^20
-ext = substr(file_path, nchar(file_path) - 2, nchar(file_path))
+file_ext = substr(file_path, nchar(file_path) - 2, nchar(file_path))
 
 assert(
   "check file size",
@@ -22,7 +24,7 @@ assert(
 
 assert(
   "check file ext",
-  ext == "tif"
+  file_ext == "tif"
 )
 
 # DEM
@@ -30,7 +32,7 @@ tmp = tempfile()
 tile_download(DEM, outdir = tmp)
 file_path = list.files(tmp, full.names = TRUE)
 file_size = file.info(file_path)$size / 2^20
-ext = substr(file_path, nchar(file_path) - 2, nchar(file_path))
+file_ext = substr(file_path, nchar(file_path) - 2, nchar(file_path))
 
 assert(
   "check file size",
@@ -39,5 +41,8 @@ assert(
 
 assert(
   "check file ext",
-  ext == "asc"
+  file_ext == "asc"
 )
+
+# test checkSHA_fun
+checkSHA_fun(refSHA = "X", fileSHA = "X")
