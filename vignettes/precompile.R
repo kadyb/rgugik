2187ded46a71ad5b2ba7fdf431e3d27baaf6ff61
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-10-02 14:03:36 +0200

    fix text

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
-#Vignettes that depend on internet access have been precompiled:
+# Vignettes that depend on internet access have been precompiled
 # Must manually move image files from rgugik/ to rgugik/vignettes/ after knit
 
 library(knitr)
