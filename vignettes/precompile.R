aa67457861e6561336661ec1d9182bb41f64f462
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-10-03 11:57:57 +0200

    Delete precompile.R

--- vignettes/precompile.R
+++
@@ -1,8 +0,0 @@
-# Vignettes that depend on internet access have been precompiled
-# Must manually move image files from rgugik/ to rgugik/vignettes/ after knit
-
-library(knitr)
-knit("vignettes/orthophotomap.Rmd.orig", "vignettes/orthophotomap.Rmd")
-
-library(devtools)
-build_vignettes()
