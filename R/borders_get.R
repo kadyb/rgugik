8fd2ddec7636fdd9bae059dee30c323e3cb9c238
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-11-11 14:17:39 +0100

    new try to stack geometries

--- R/borders_get.R
+++ R/borders_get.R
@@ -27,7 +27,7 @@ borders_get = function(voivodeship = NULL, county = NULL, commune = NULL,
     request = "GetVoivodeshipById"
     df_names = rgugik::voivodeship_names
     sel_vector = df_names[["NAME_PL"]] %in% voivodeship
-    TERYT = df_names[sel_vector, "TERC", drop = TRUE]
+    ID = df_names[sel_vector, "TERC", drop = TRUE]
 
   }
 
@@ -36,7 +36,7 @@ borders_get = function(voivodeship = NULL, county = NULL, commune = NULL,
     request = "GetCountyById"
     df_names = rgugik::county_names
     sel_vector = df_names[["NAME"]] %in% county
-    TERYT = df_names[sel_vector, "TERYT", drop = TRUE]
+    ID = df_names[sel_vector, "TERYT", drop = TRUE]
 
   }
 
@@ -45,7 +45,7 @@ borders_get = function(voivodeship = NULL, county = NULL, commune = NULL,
     request = "GetCommuneById"
     df_names = rgugik::commune_names
     sel_vector = df_names[["NAME"]] %in% commune
-    TERYT = df_names[sel_vector, "TERYT", drop = TRUE]
+    ID = df_names[sel_vector, "TERYT", drop = TRUE]
 
   }
 
@@ -65,32 +65,31 @@ borders_get = function(voivodeship = NULL, county = NULL, commune = NULL,
 
     if (nchar(TERYT) == 7) {
       # process commune TERYT to specific form (XXXXXX_X)
-      TERYT = paste0(substr(TERYT, 1, 6), "_", substr(TERYT, 7, 7))
+      ID = paste0(substr(TERYT, 1, 6), "_", substr(TERYT, 7, 7))
       request = "GetCommuneById"
     }
 
   }
 
   result = "geom_wkb"
-  # geom_stack = list()
-  geom_stack = st_polygon()
+  geometry = st_sfc(st_polygon(), crs = 2180)
 
-  for (i in seq_len(length(TERYT))) {
+  for (i in seq_len(length(ID))) {
 
     prepared_URL = paste0("https://uldk.gugik.gov.pl/?request=", request,
-                          "&id=", TERYT[i],
+                          "&id=", ID[i],
                           "&result=", result)
 
     output = readLines(prepared_URL, warn = FALSE)
     output = output[2]
     wkb = structure(list(output), class = "WKB")
-    geom = sf::st_as_sfc(wkb, EWKB = TRUE, crs = 2180)
-    # geom_stack = c(geom_stack, geom)
-    geom_stack = rbind(geom_stack, geom)
+    single_geom = sf::st_as_sfc(wkb, EWKB = TRUE, crs = 2180)
+    geometry = c(geometry, single_geom)
 
   }
 
-  df_geom = sf::st_sf(geom_stack)
+  df_geom = sf::st_sf(geometry)
+  df_geom = df_geom[-1, ] # drop empty geometry
 
   # geometry is returned in EPSG: 2180
   return(df_geom)
