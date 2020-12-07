57b3378c37f50613fa6f6457166f9b51512341fa
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2020-12-07 11:41:10 +0100

    set old wd in 'PRECOMPILE.R'

--- vignettes/PRECOMPILE.R
+++ vignettes/PRECOMPILE.R
@@ -1,7 +1,9 @@
 # Vignettes that depend on internet access have been precompiled
 
 library(knitr)
+oldwd = getwd()
 setwd("vignettes/")
 knit("orthophotomap.Rmd.orig", "orthophotomap.Rmd")
 knit("DEM.Rmd.orig", "DEM.Rmd")
 knit("topodb.Rmd.orig", "topodb.Rmd")
+setwd(oldwd)
