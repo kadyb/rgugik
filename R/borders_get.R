fb08a0ea8ff3d3499461ad3afadc7b1442c0d570
Jakub Nowosad <Nowosad@users.noreply.github.com>
2022-05-29 17:24:58 -0400

    adds pl borders (#89)
    * adds pl borders
    * add details
    
    Co-authored-by: Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>

--- R/borders_get.R
+++ R/borders_get.R
@@ -10,6 +10,9 @@
 #'
 #' @return a sf data.frame (EPSG: 2180)
 #'
+#' @details If all arguments are NULL (default),
+#' the boundary of Poland will be returned.
+#'
 #' @export
 #'
 #' @examples
@@ -17,88 +20,95 @@
 #' voivodeship_geom = borders_get(voivodeship = "lubuskie") # 494 KB
 #' county_geom = borders_get(county = "Sopot") # 18 KB
 #' commune_geom = borders_get(commune = c("Hel", "Krynica Morska")) # 11 KB
+#' poland_geom = borders_get() # 1124.3 KB
 #' }
 borders_get = function(voivodeship = NULL, county = NULL, commune = NULL,
                        TERYT = NULL) {
 
-  if (!is.null(voivodeship)) {
+  if (all(is.null(c(voivodeship, county, commune, TERYT)))) {
+    poland = borders_get(voivodeship = rgugik::voivodeship_names[, 1])
+    return(sf::st_union(poland, is_coverage = TRUE))
+  } else {
 
-    request = "GetVoivodeshipById"
-    df_names = rgugik::voivodeship_names
-    sel_vector = df_names[["NAME_PL"]] %in% voivodeship
-    ID = df_names[sel_vector, "TERC", drop = TRUE]
+    if (!is.null(voivodeship)) {
 
-  }
-
-  if (!is.null(county)) {
-
-    request = "GetCountyById"
-    df_names = rgugik::county_names
-    sel_vector = df_names[["NAME"]] %in% county
-    ID = df_names[sel_vector, "TERYT", drop = TRUE]
-
-  }
-
-  if (!is.null(commune)) {
-
-    request = "GetCommuneById"
-    df_names = rgugik::commune_names
-    sel_vector = df_names[["NAME"]] %in% commune
-    ID = df_names[sel_vector, "TERYT", drop = TRUE]
-
-  }
-
-  # TERYT and ID are the same but inside fun must be as different objects
-  if (!is.null(TERYT)) {
-
-    if (!all(nchar(TERYT) %in% c(2, 4, 7))) {
-      stop("'TERYT' length should be 2, 4 or 7")
-    }
-
-    if (all(nchar(TERYT) == 2)) {
       request = "GetVoivodeshipById"
-      ID = TERYT
+      df_names = rgugik::voivodeship_names
+      sel_vector = df_names[["NAME_PL"]] %in% voivodeship
+      ID = df_names[sel_vector, "TERC", drop = TRUE]
+
     }
 
-    if (all(nchar(TERYT) == 4)) {
+    if (!is.null(county)) {
+
       request = "GetCountyById"
-      ID = TERYT
+      df_names = rgugik::county_names
+      sel_vector = df_names[["NAME"]] %in% county
+      ID = df_names[sel_vector, "TERYT", drop = TRUE]
+
     }
 
-    if (all(nchar(TERYT) == 7)) {
-      # process commune TERYT to specific form (XXXXXX_X)
-      ID = paste0(substr(TERYT, 1, 6), "_", substr(TERYT, 7, 7))
+    if (!is.null(commune)) {
+
       request = "GetCommuneById"
+      df_names = rgugik::commune_names
+      sel_vector = df_names[["NAME"]] %in% commune
+      ID = df_names[sel_vector, "TERYT", drop = TRUE]
+
     }
 
-  }
+    # TERYT and ID are the same but inside fun must be as different objects
+    if (!is.null(TERYT)) {
 
-  result = "geom_wkb"
-  geometry = sf::st_sfc(sf::st_polygon(), crs = 2180)
+      if (!all(nchar(TERYT) %in% c(2, 4, 7))) {
+        stop("'TERYT' length should be 2, 4 or 7")
+      }
 
-  for (i in seq_len(length(ID))) {
+      if (all(nchar(TERYT) == 2)) {
+        request = "GetVoivodeshipById"
+        ID = TERYT
+      }
 
-    prepared_URL = paste0("https://uldk.gugik.gov.pl/?request=", request,
-                          "&id=", ID[i],
-                          "&result=", result)
+      if (all(nchar(TERYT) == 4)) {
+        request = "GetCountyById"
+        ID = TERYT
+      }
 
-    output = tryGet(readLines(prepared_URL, warn = FALSE))
+      if (all(nchar(TERYT) == 7)) {
+        # process commune TERYT to specific form (XXXXXX_X)
+        ID = paste0(substr(TERYT, 1, 6), "_", substr(TERYT, 7, 7))
+        request = "GetCommuneById"
+      }
 
-    if (any(output %in% c("error", "warning"))) {
-      return(invisible("connection error"))
     }
 
-    output = output[2]
-    wkb = structure(list(output), class = "WKB")
-    single_geom = sf::st_as_sfc(wkb, EWKB = TRUE, crs = 2180)
-    geometry = c(geometry, single_geom)
+    result = "geom_wkb"
+    geometry = sf::st_sfc(sf::st_polygon(), crs = 2180)
 
+    for (i in seq_len(length(ID))) {
+
+      prepared_URL = paste0("https://uldk.gugik.gov.pl/?request=", request,
+                            "&id=", ID[i],
+                            "&result=", result)
+
+      output = tryGet(readLines(prepared_URL, warn = FALSE))
+
+      if (any(output %in% c("error", "warning"))) {
+        return(invisible("connection error"))
+      }
+
+      output = output[2]
+      wkb = structure(list(output), class = "WKB")
+      single_geom = sf::st_as_sfc(wkb, EWKB = TRUE, crs = 2180)
+      geometry = c(geometry, single_geom)
+
+    }
+
+    df_geom = sf::st_sf(geometry)
+    df_geom = df_geom[-1, ] # drop empty geometry
+    df_geom = cbind(df_geom, TERYT = ID)
+
+    # geometry is returned in EPSG: 2180
+    return(df_geom)
   }
-
-  df_geom = sf::st_sf(geometry)
-  df_geom = df_geom[-1, ] # drop empty geometry
-  df_geom = cbind(df_geom, TERYT = ID)
-
-  # geometry is returned in EPSG: 2180
-  return(df_geom)
 }
