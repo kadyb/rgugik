6cdfdac82dcaf06997264eee8d7ad55cf3ca1e56
Grzegorz Sapijaszko <grzegorz@sapijaszko.net>
2024-12-11 21:21:48 +0100

    egib layers (#102)
    * egib layers
    
    * egib_download()

---
+++ R/egib_layers.R
@@ -0,0 +1,14 @@
+#' @name egib_layers
+#' @title Available urls and layers of Land and building register (EGiB)
+#'
+#' @description The data frame contains the names of counties,
+#' their identifiers (TERYT, 4 characters), URL ofthe EGiB data (WFS)
+#' and the names of available layers.
+#'
+#' @importFrom utils data
+#'
+#' @docType data
+#' @keywords dataset EGiB
+#' @examples
+#' egib_layers
+"egib_layers"
